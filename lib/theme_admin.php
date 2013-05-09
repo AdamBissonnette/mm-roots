761074e68b77fa032ce34514fb0b2bb53d86dec3
Adam Bissonnette <adam@medimanifesto.com>
2013-05-09 17:10:13 -0600

    Added Service Page html
    Added function to create selects

--- lib/theme_admin.php
+++ lib/theme_admin.php
@@ -9,6 +9,13 @@
 			<fieldset>
 				<legend>Options</legend>
 				<div class="control-group">
+					<label class="control-label" for="<?php echo $this->_setting_prefix . '_service_page'; ?>">Service Page</label>
+					<div class="controls">
+						<input id="<?php echo $this->_setting_prefix . '_service_page'; ?>" type="text" class="input-large req" name="<?php echo $this->_setting_prefix . '_service_page'; ?>" value="<?php echo($this->_settings[$this->_setting_prefix . '_service_page']); ?>" />
+					</div>
+				</div>
+				
+				<div class="control-group">
 					<label class="control-label" for="<?php echo $this->_setting_prefix . '_service_category'; ?>">Service Category</label>
 					<div class="controls">
 						<input id="<?php echo $this->_setting_prefix . '_service_category'; ?>" type="text" class="input-large req" name="<?php echo $this->_setting_prefix . '_service_category'; ?>" value="<?php echo($this->_settings[$this->_setting_prefix . '_service_category']); ?>" />
@@ -31,3 +38,35 @@
 		</div>
 	</div>
 </div>
+
+<?php
+/* function createSelectOptionsFromArray($optionArray, $id)
+{
+	$output = '<select id="" name="">';
+	$optionTemplate = '<option value="%s">%s</option>\n';
+	$output .= sprintf($optionTemplate, "", "Select an Option");
+	foreach ($optionArray as $key = $value)
+	{
+		$output .= sprintf($optionTemplate, $key, $value);
+	}
+	
+	return $output;
+}
+
+function getCategorySelect()
+{
+	$categories;//getCategories;
+	
+	$catArray = array();
+	$i = 0;
+	foreach ($categories as $category)
+	{
+		$catArray = array(["key"] = $category->ID,
+		$catArray["value"] = $category->name
+		);
+		
+	}
+}
+*/
+
+?>
