94afcdba5984b3c8427c574884f1b4c01ea44d6a
Adam B <Adam@Adams-iMac.local>
2013-05-16 16:56:48 -0600

    Pulling homepage jumbotron from category
    Using 'image' post meta for the related image

--- templates/content-jumbotron.php
+++ templates/content-jumbotron.php
@@ -1,15 +1,47 @@
+<?php
+global $MM_Roots_Optical;
+
+$jumbotron = get_posts( "category=" . $MM_Roots_Optical->_settings["mm_op_jumbotron_category"] . "&numberposts=1" );
+$post = $jumbotron[0];
+
+$blurb = get_post_meta($post->ID, "blurb", true);
+  		
+if ($blurb == null)
+{
+	$blurb = wp_trim_words($post->post_content, 25, "...");
+}
+
+$readmoretext = get_post_meta($post->ID, "readmoretext", true);
+
+if ($readmoretext == null)
+{
+	$readmoretext = "Read More";
+}
+
+$image = get_post_meta($post->ID, "image", true);
+
+if ($image == null)
+{
+	$image = "assets/img/ipad.png";
+}
+
+?>
+
+
 <section class="section-content section-jumbotron" id="section-jumbotron">
     <div class="container">
       <div class="row">
         <div class="span5 offset1">
-          <img alt="" src="assets/img/ipad.png">
+          <img alt="" src="<?php echo $image; ?>">
         </div>
         <div class="span5 _offset1">
           <div class="slogan">
-            <h1>BOOTSTRAP #RWD</h1>
-            <p class="lead">A really responsive theme written in HTML5/CSS3 with the power of HAML/LESS and a fistful of cool plugins!</p>
+            <h1><?php echo $post->post_title; ?></h1>
+            <p class="lead"><?php
+            	echo $blurb;
+             ?></p>
             <p>
-              <a class="btn btn-large btn-primary" href="features.html">Features</a>
+              <a class="btn btn-large btn-primary" href="<?php echo get_permalink($post); ?>"><?php echo $readmoretext; ?></a>
             </p>
           </div>
         </div>
