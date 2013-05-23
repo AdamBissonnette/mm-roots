bcb9b093a5c58b113886d758cd3d8cc440ff2684
Adam B <Adam@Adams-iMac.local>
2013-05-23 14:23:23 -0600

    Updated 404 page and Search Form

--- templates/searchform.php
+++ templates/searchform.php
@@ -1,4 +1,5 @@
 <form role="search" method="get" id="searchform" class="form-search" action="<?php echo home_url('/'); ?>">
   <label class="hide" for="s"><?php _e('Search for:', 'roots'); ?></label>
+  <span class="add-on"><i class="icon-search"></i></span>
   <input type="text" value="<?php if (is_search()) { echo get_search_query(); } ?>" name="s" id="s" class="search-query" placeholder="<?php _e('Search', 'roots'); ?> <?php bloginfo('name'); ?>">
 </form>
