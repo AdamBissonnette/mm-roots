e780dd9094ecce1c1ec6fd83c511f21e39f198a5
Adam B <Adam@Adams-iMac.local>
2013-05-29 18:12:26 -0600

    Added built in fields for post / page meta

--- lib/theme_core.php
+++ lib/theme_core.php
@@ -31,7 +31,7 @@ class MM_Roots
 		add_action('wp_ajax_nopriv_do_ajax', array(&$this, '_save') );
 		add_action('wp_ajax_do_ajax', array(&$this, '_save') );
     }
-    
+
     static function MM_Roots_install() {
     	global $wpdb;
     	require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -56,13 +56,15 @@ class MM_Roots
         
         wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
         wp_enqueue_style('admin', get_template_directory_uri() . '/assets/css/mm_roots_admin.css', false, null);
-        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.min.js', false, null);
+        wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/js/vendor/jquery-1.9.1.min.js', false, null);
+        wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/plugins.js', false, null);
+  		wp_enqueue_script('formtools', get_template_directory_uri() . '/assets/js/formtools.js', false, null);
   		wp_enqueue_script('admin', get_template_directory_uri() . '/assets/js/mm_roots_admin.js', false, null);
         
         include_once('theme_admin_data.php');
 		include_once('theme_admin_ui.php');
     }
-    
+
     function check_user_capability()
     {
         if ( is_super_admin() || current_user_can('manage_options') ) return true;
@@ -171,3 +173,129 @@ function MM_Roots_Init()
     global $MM_Roots;
     $MM_Roots = new MM_Roots();
 }
+
+function page_metabox(){
+	add_meta_box("mm_page_tagline", "Page Meta", "page_meta", "page");
+}
+
+function page_meta(){
+	global $post;
+
+	$tagline = get_post_meta($post->ID,'tagline',true);
+	$readmoreid = get_post_meta($post->ID,'readmoreid',true);
+	$blurb = get_post_meta($post->ID,'blurb',true);
+	$icon = get_post_meta($post->ID,'icon',true);
+	$image = get_post_meta($post->ID,'image',true);
+
+	?>
+
+	<?php wp_nonce_field( 'mm_nonce', 'mm_nonce' ); ?>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="tagline" style="color: #000; font-weight: bold;">Tagline</label>
+		<input type="text" value="<?php echo esc_attr( $tagline ); ?>" id="tagline" name="tagline" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="readmoreid" style="color: #000; font-weight: bold;">Read More ID</label>
+		<input type="text" value="<?php echo esc_attr( $readmoreid ); ?>" id="readmoreid" name="readmoreid" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="blurb" style="color: #000; font-weight: bold;">Blurb</label>
+		<input type="text" value="<?php echo esc_attr( $blurb ); ?>" id="blurb" name="blurb" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="icon" style="color: #000; font-weight: bold;">Icon</label>
+		<input type="text" value="<?php echo esc_attr( $icon ); ?>" id="icon" name="icon" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="image" style="color: #000; font-weight: bold;">Image Url</label>
+		<input type="text" value="<?php echo esc_attr( $image ); ?>" id="image" name="image" size="20" />
+		<p>Note: Use this if you want to link to an external URL as the featured image</p>
+	</div>
+
+	<?php
+}
+
+add_action("admin_init", "page_metabox");
+
+
+function post_metabox(){
+	add_meta_box("mm_post_tagline", "Post Meta", "post_meta", "post");
+}
+
+function post_meta(){
+	global $post;
+
+	$tagline = get_post_meta($post->ID,'tagline',true);
+	$blurb = get_post_meta($post->ID,'blurb',true);
+	$icon = get_post_meta($post->ID,'icon',true);
+	$image = get_post_meta($post->ID,'image',true);
+
+	?>
+
+	<?php wp_nonce_field( 'mm_nonce', 'mm_nonce' ); ?>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="tagline" style="color: #000; font-weight: bold;">Tagline</label>
+		<input type="text" value="<?php echo esc_attr( $tagline ); ?>" id="tagline" name="tagline" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="blurb" style="color: #000; font-weight: bold;">Blurb</label>
+		<input type="text" value="<?php echo esc_attr( $blurb ); ?>" id="blurb" name="blurb" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="icon" style="color: #000; font-weight: bold;">Icon</label>
+		<input type="text" value="<?php echo esc_attr( $icon ); ?>" id="icon" name="icon" size="20" />
+	</div>
+
+	<div style="margin: 13px 0 11px 4px;">
+		<label for="image" style="color: #000; font-weight: bold;">Image Url</label>
+		<input type="text" value="<?php echo esc_attr( $image ); ?>" id="image" name="image" size="20" />
+		<p>Note: Use this if you want to link to an external URL as the featured image</p>
+	</div>
+
+	<?php
+}
+
+add_action("admin_init", "post_metabox");
+
+function mm_save_details( $post_id, $post ){
+	global $pagenow;
+	
+	if ( 'post.php' != $pagenow ) return $post_id;
+	
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+		return $post_id;
+
+	if ( ! isset( $_POST['mm_nonce'] ) || ! wp_verify_nonce( $_POST['mm_nonce'], 'mm_nonce' ) )
+        return $post_id;
+
+	switch ($post->post_type) {
+		case 'post':
+			update_post_meta( $post_id, "blurb", $_POST["blurb"] );
+			update_post_meta( $post_id, "tagline", $_POST["tagline"] );
+			update_post_meta( $post_id, "icon", $_POST["icon"] );
+			update_post_meta( $post_id, "image", $_POST["image"] );
+
+			break;
+		case 'page':
+			update_post_meta( $post_id, "blurb", $_POST["blurb"] );
+			update_post_meta( $post_id, "tagline", $_POST["tagline"] );
+			update_post_meta( $post_id, "icon", $_POST["icon"] );
+			update_post_meta( $post_id, "image", $_POST["image"] );
+			update_post_meta( $post_id, "readmoreid", $_POST["readmoreid"] );
+
+			break;
+		default:
+			//Do Nothing
+			break;
+	}
+}
+
+add_action( 'save_post', 'mm_save_details', 10, 2 );
