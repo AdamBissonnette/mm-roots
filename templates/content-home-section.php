2fc123486bf21d5b3dc585e5e66b9165bd6d10de
Adam B <Adam@Adams-iMac.local>
2013-06-03 19:30:18 -0600

    Added home section

---
+++ templates/content-home-section.php
@@ -0,0 +1,97 @@
+<?php
+  
+?>
+
+<section id="features-section" class="section-2" style="background-position: 50% 55px;">
+  <div class="bg-wraper parallax-point-event">
+  <div class="container">
+    <div class="row-fluid">
+      <header class="">
+        <div class="text-center page-header">
+          <h3 class="">
+            <span class="white"><!-- WHITE SPACE-->
+              WHAT WE ARE <span class="main-color">OFFER</span> 
+            </span> 
+          </h3>
+        </div>
+      </header>
+    </div>
+</div><!-- /SERVICE HEADER-->
+  <div class="container">
+    <div class="row-fluid">
+    <!-- FEATURE ITEM-->
+      <div class="span3 effect-box-1 active">
+        <span class="label label-warning lines-bg-color text-center">
+                <i class="fa-icon-fullscreen fa-icon-large"></i>
+        </span>
+        <h4 class="features-title"> 
+          <span class="main-color">Responsive</span> Layout
+        </h4>
+        <p class=""> We combine creative forces with systematic activists. All ideas require implementation and our multi-disciplined netproject fosters a wide array of abilities that empower visionaries and entrepreneurs to lead meaningful brands.</p>
+  </div><!-- END FEATURE ITEM-->
+    
+    
+    <!-- FEATURE ITEM-->
+    <div class="span3 effect-box-2 active">
+      <span class="label label-warning lines-bg-color text-center">
+      <i class="fa-icon-tint fa-icon-large"></i>
+      </span>
+      <h4 class="features-title"> 
+        <span class="main-color">Designer  </span>Friendly
+      </h4>
+      <p class=""> We combine creative forces with systematic activists. All ideas require implementation and our multi-disciplined netproject fosters a wide array of abilities that empower visionaries and entrepreneurs to lead meaningful brands.</p>
+    </div><!-- END FEATURE ITEM-->
+    
+    <!-- FEATURE ITEM-->
+    <div class="span3 effect-box-3 active"> 
+      <span class="label label-warning lines-bg-color text-center">
+      <i class="fa-icon-thumbs-up fa-icon-large"></i>
+      </span>
+      <h4 class="features-title"> 
+        <span class="main-color">Faster</span> Setup
+      </h4>
+      <p class=""> Inspired Thinking. Ask smarter questions, push for innovative solutions and believe in the power of creativity. Be consistently open and honest. Be true to ourselves and our values. Find, stimulate and maintain the best minds.</p>
+    </div>
+    <!-- END FEATURE ITEM-->
+    <!-- FEATURE ITEM-->
+    <div class="span3 effect-box-4 active"> 
+      <span class="label label-warning lines-bg-color text-center">
+      <i class="fa-icon-spinner fa-icon-spin fa-icon-large "></i>
+      </span>
+      <h4 class="features-title">
+        <span class="main-color">Modern</span> features
+      </h4>
+      <p class=""> Inspired Thinking. Ask smarter questions, push for innovative solutions and believe in the power of creativity. Be consistently open and honest. Be true to ourselves and our values. Find, stimulate and maintain the best minds.</p>
+    </div>
+    <!-- FEATURE ITEM-->
+    </div><!--/ ROW-FLUID-->
+    <hr>
+  
+  
+  <div class="row-fluid">
+    <div class="well">
+      <div class="well-wrapp">
+        <p class="lead text-large text-center">
+          
+            <span class="main-color"> 
+              Scroll&amp;Strap
+            </span> - modern Bootstrap onepager with parallax effect and smooth scrolling
+        </p>
+  <span class="btn-box">        
+    <a href="#" class="btn btn-icon-large  btn-large  btn-action btn-block-third">
+    <i class="fa-icon-download-alt fa-icon-large"></i> 
+    Buy now!
+    </a>
+      <i class="or">- or - </i>
+      <span id="nav3">
+        <a href="#slogan-section-2" class="btn btn-icon-large  btn-large  btn-green-alt btn-block-third"><i class="fa-icon-info-sign fa-icon-large"></i> Show me more!</a>
+      </span>
+  </span>
+        
+        </div>  
+      </div>
+    </div><!-- /row-fluid-->
+  </div><!--/CONTAINER-->
+</div><!-- / BG WRAPER-->
+
+</section>
