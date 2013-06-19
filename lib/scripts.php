30a497f5df514d86ef16940f718e5e85071a37f1
Adam B <Adam@Adams-iMac.local>
2013-06-19 16:46:55 -0600

    Modified old comments

--- lib/scripts.php
+++ lib/scripts.php
@@ -1,18 +1,6 @@
 <?php
 /**
- * Enqueue scripts and stylesheets
- *
- * Enqueue stylesheets in the following order:
- * 1. /theme/assets/css/bootstrap.css
- * 2. /theme/assets/css/bootstrap-responsive.css
- * 3. /theme/assets/css/app.css
- * 4. /child-theme/style.css (if a child theme is activated)
- *
- * Enqueue scripts in the following order:
- * 1. jquery-1.9.1.min.js via Google CDN
- * 2. /theme/assets/js/vendor/modernizr-2.6.2.min.js
- * 3. /theme/assets/js/plugins.js (in footer)
- * 4. /theme/assets/js/main.js    (in footer)
+ * Enqueue stylesheets then scripts
  */
 function roots_scripts() {
   wp_enqueue_style('styles', get_template_directory_uri() . '/assets/css/styles.css', false, null);
