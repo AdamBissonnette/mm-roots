696711018aa3ed615da7eb3ae6f1be152edc35ca
Adam B <Adam@Adams-iMac.local>
2013-08-20 16:04:41 -0600

    Styling / Class Modifications

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -17,7 +17,7 @@
         <nav class="nav-collapse collapse" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
-            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav pull-right'));
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
           endif;
         ?>
       </nav>
